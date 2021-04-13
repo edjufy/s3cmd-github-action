334b35a58157821791e36ce5d2dd67b98f8a1aee
Laurenz Glück <l.glueck@edjufy.com>
2021-04-13 10:47:03 +0200

    adds some comments to main.sh script

--- scripts/main.sh
+++ scripts/main.sh
@@ -10,13 +10,17 @@ secretKey=$3
 hostBase=$4
 bucketLocation=$5
 
+## copy preset config into place
 cp /.s3cfg /github/home/.s3cfg
 
+## replace placeholder in config
 sed -i s/{{BUCKET_LOCATION}}/${bucketLocation}/g /github/home/.s3cfg
 sed -i s/{{HOST_BASE}}/${hostBase}/g /github/home/.s3cfg
 sed -i s/{{ACCESS_KEY}}/${accessKey}/g /github/home/.s3cfg
 sed -i s/{{SECRET_KEY}}/${secretKey}/g /github/home/.s3cfg
 
+## run s3cmd command
 s3cmd ${s3cmdCommand}
 
+## delete config
 rm -rf /github/home/.s3cfg
